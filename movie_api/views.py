28b39900040587eb5b0a48371c16bd6ce0ce23c7
asifjalaldev <jalalasif92211@gmail.com>
2024-02-23 21:41:10 +0500

    set up urls and views of list and detail movie

--- movie_api/views.py
+++ movie_api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def movie_list(request):
+    return HttpResponse('testing movie list')
+def movie_detail(request, pk):
+    return HttpResponse(f'testing movie detail {pk}')
