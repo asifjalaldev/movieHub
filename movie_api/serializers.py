b2b524516180b03a96951f92102dfde21a6a0d4b
asifjalaldev <jalalasif92211@gmail.com>
2024-03-02 00:24:05 +0500

    crud using class base views

--- movie_api/serializers.py
+++ movie_api/serializers.py
@@ -2,51 +2,62 @@ from rest_framework import serializers
 
 from .models import WatchList, StreamPlatform
 
-class WatchListSerializer(serializers.Serializer):
-    id= serializers.IntegerField(read_only=True)
-    title=serializers.CharField(max_length=50)
-    storyLine=serializers.CharField(max_length=100)
-    # platform=serializers.ForeignKey(StreamPlatform, on_delete=models.CASCADE)
-    active=serializers.BooleanField(default=True)
-    created=serializers.DateTimeField()
+# class WatchListSerializer(serializers.Serializer):
+#     id= serializers.IntegerField(read_only=True)
+#     title=serializers.CharField(max_length=50)
+#     storyLine=serializers.CharField(max_length=100)
+#     # platform=serializers.ForeignKey(StreamPlatform, on_delete=models.CASCADE)
+#     active=serializers.BooleanField(default=True)
+#     created=serializers.DateTimeField()
     
-    def create(self, validated_data):
-        """
-        create and return a new 'watchlist' instance, given the validated data
-        """
-        return WatchList.objects.create(**validated_data)
+#     def create(self, validated_data):
+#         """
+#         create and return a new 'watchlist' instance, given the validated data
+#         """
+#         return WatchList.objects.create(**validated_data)
    
-    def update(self, instance, validated_data):
-        """
-        update and return an existing 'watchlist' instance, given the validated data
-        """
-        instance.title=validated_data.get('title', instance.title)
-        instance.storyLine=validated_data.get('storyLine', instance.storyLine)
-        instance.active=validated_data.get('active', instance.active)
-        instance.created=validated_data.get('created', instance.created)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         """
+#         update and return an existing 'watchlist' instance, given the validated data
+#         """
+#         instance.title=validated_data.get('title', instance.title)
+#         instance.storyLine=validated_data.get('storyLine', instance.storyLine)
+#         instance.active=validated_data.get('active', instance.active)
+#         instance.created=validated_data.get('created', instance.created)
+#         instance.save()
+#         return instance
     
-class StreamPlatformSerializer(serializers.Serializer):
-    id= serializers.IntegerField(read_only=True)
-    name=serializers.CharField(max_length=50)
-    about=serializers.CharField(max_length=50)
-    website=serializers.URLField(max_length=100)
+# class StreamPlatformSerializer(serializers.Serializer):
+#     id= serializers.IntegerField(read_only=True)
+#     name=serializers.CharField(max_length=50)
+#     about=serializers.CharField(max_length=50)
+#     website=serializers.URLField(max_length=100)
 
-    def create(self, validated_data):
-        """
-        create instance of streamplatform 
-        """
-        return StreamPlatform.objects.create(**validated_data)
+#     def create(self, validated_data):
+#         """
+#         create instance of streamplatform 
+#         """
+#         return StreamPlatform.objects.create(**validated_data)
     
-    def update(self, instance, validated_data):
-        """
-        update and return an existing 'streamPlatform' instance, given the validated data
-        """
-        instance.name=validated_data.get('name', instance.name)
-        instance.about=validated_data.get('about', instance.about)
-        instance.website=validated_data.get('website', instance.website)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         """
+#         update and return an existing 'streamPlatform' instance, given the validated data
+#         """
+#         instance.name=validated_data.get('name', instance.name)
+#         instance.about=validated_data.get('about', instance.about)
+#         instance.website=validated_data.get('website', instance.website)
+#         instance.save()
+#         return instance
+#  class base view---------------------------------
+
+class WatchListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=WatchList
+        fields='__all__'
+        
+class StreamPlatformSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=StreamPlatform
+        fields='__all__'
     
     
--- movie_api/urls.py
+++ movie_api/urls.py
@@ -1,9 +1,18 @@
 
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('list/', views.movie_list, name='movie-list'),
-    path('list/<int:pk>', views.movie_detail, name='movie-detail'),
-     path('stream/', views.stream_list, name='stream-platform'),
-    path('stream/<int:pk>', views.stream_detail, name='stream-platform-detail'),
+    # path('list/', views.movie_list, name='movie-list'),
+    # path('list/<int:pk>', views.movie_detail, name='movie-detail'),
+    #  path('stream/', views.stream_list, name='stream-platform'),
+    # path('stream/<int:pk>', views.stream_detail, name='stream-platform-detail'),
+# class base views urls---------------
+    path('stream/', views.Stream.as_view(), name='stream-platform'),
+    path('stream/<int:pk>', views.StreamDetail.as_view(), name='stream-detail'),
+    path('list/', views.watchListView.as_view(), name='movie-list'),
+    path('list/<int:pk>', views.watchListDetailView.as_view(), name='movie-detail'),
+
+    
+
 ]
