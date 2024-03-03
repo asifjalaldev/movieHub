389b31fdbc00510f35223ddad69bcd30e26305d5
asifjalaldev <jalalasif92211@gmail.com>
2024-03-03 11:52:44 +0500

    fix urls for hyperlink serializers

--- movie_api/urls.py
+++ movie_api/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
     #  path('stream/', views.stream_list, name='stream-platform'),
     # path('stream/<int:pk>', views.stream_detail, name='stream-platform-detail'),
 # class base views urls---------------
-    path('stream/', views.Stream.as_view(), name='stream-platform'),
-    path('stream/<int:pk>', views.StreamDetail.as_view(), name='stream-detail'),
-    path('list/', views.watchListView.as_view(), name='movie-list'),
-    path('list/<int:pk>', views.watchListDetailView.as_view(), name='movie-detail'),
+    # ------to use hyperlink serializer as bas class , urls must be in this format modelName-list/detail
+    path('stream/', views.Stream.as_view(), name='StreamPlatform-list'),
+    path('stream/<int:pk>', views.StreamDetail.as_view(), name='StreamPlatform-detail'),
+    path('list/', views.watchListView.as_view(), name='WatchList-list'),
+    path('list/<int:pk>', views.watchListDetailView.as_view(), name='WatchList-detail'),
     path('',views.api_root),
     
 
